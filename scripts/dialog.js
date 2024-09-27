5fef5ba2fc562af27b74d98e7cf0e3f54986db19
Chiri Francisco <franchiri00@gmail.com>
2024-09-27 19:44:32 -0300

    vs

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -10,13 +10,17 @@ function abrirModal() {
     modal.classList.remove('hiding');
     modal.classList.add('showing');
     modal.showModal();
+    document.body.style.overflow = 'hidden'; // Deshabilitar el scroll
 }
 
 // Función para cerrar el modal con animación
 function cerrarModal() {
     modal.classList.remove('showing');
     modal.classList.add('hiding');
-    setTimeout(() => modal.close(), 600);  // Espera a que termine la animación antes de cerrar
+    setTimeout(() => {
+        modal.close();
+        document.body.style.overflow = ''; // Rehabilitar el scroll
+    }, 600);  // Espera a que termine la animación antes de cerrar
 }
 
 // Evento para abrir modal al hacer clic en los botones correspondientes
@@ -29,17 +33,17 @@ botonCerrarModal.addEventListener('click', cerrarModal);
 // Evento para cerrar modal haciendo clic fuera de él
 modal.addEventListener('click', e => {
     const dialogDimensions = modal.getBoundingClientRect();
-    const isClickOutside = e.clientX < dialogDimensions.left || 
-                           e.clientX > dialogDimensions.right || 
-                           e.clientY < dialogDimensions.top || 
-                           e.clientY > dialogDimensions.bottom;
-    
+    const isClickOutside = e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom;
+
     if (isClickOutside) {
         cerrarModal();  // Primero dispara la animación de cierre
     }
 });
 
 // Evitar que el clic dentro del modal cierre el modal
-document.getElementById('modal').addEventListener('click', e => {
+modal.addEventListener('click', e => {
     e.stopPropagation();  // Detiene la propagación del evento para evitar el cierre al hacer clic dentro del modal
 });
